f576f8b052ffdf91d5c12a36b2f0b41bb26f382c
Nathan C <nathan.r.comstock@gmail.com>
2020-01-21 08:55:28 -0700

    Add User Message Promt

---
+++ MoreOperators-1-21-20/MoreOperators.vb
@@ -0,0 +1,21 @@
+﻿'Nathan Comstock
+'RCET0265
+'Spring 2020
+'MoreOptions
+
+Option Strict On
+Option Explicit On
+
+Module MoreOperators
+
+    Sub Main()
+        'Promt User for a Message.
+        Dim userMessage As String
+        'userMessage = "Hello"
+        Console.WriteLine("Enter a message.")
+        userMessage = Console.ReadLine()
+        Console.WriteLine("You have selected " & userMessage & "Excellent work.")
+        Console.ReadLine()
+    End Sub
+
+End Module
