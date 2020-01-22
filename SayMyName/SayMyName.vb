4248baf55304956488da956bad5e6a96d92e2006
Nathan C <nathan.r.comstock@gmail.com>
2020-01-22 08:18:23 -0700

    Say My Name Assignment 1/22/2020

---
+++ SayMyName/SayMyName.vb
@@ -0,0 +1,20 @@
+﻿'Nathan Comstock
+'RCET0265
+'Spring 2020
+'SayMyName
+'https://github.com/comsnath/NC-VS-S20
+
+Option Explicit On
+Option Strict On
+
+Module SayMyName
+
+    Sub Main()
+        Dim userName As String
+        Console.WriteLine("Hello user... What is thou name?")
+        userName = Console.ReadLine()
+        Console.WriteLine("Hello " & userName & "! My name is HAL, thank you for awakening me from my deep slumber!")
+        Console.ReadLine()
+    End Sub
+
+End Module
