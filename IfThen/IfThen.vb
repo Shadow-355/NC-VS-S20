a50c0f269a9116f04d001d038656aee7773d0cde
Nathan C <nathan.r.comstock@gmail.com>
2020-01-23 08:40:34 -0700

    Simple If Then Solution

---
+++ IfThen/IfThen.vb
@@ -0,0 +1,35 @@
+﻿'Nathan Comstock
+'RCET0265
+'Spring 2020
+'IfThen
+'https://github.com/comsnath/NC-VS-S20
+
+Module IfThen
+
+    Sub Main()
+
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+
+        firstNumber = 3
+        secondNumber = 3
+
+
+        If firstNumber > secondNumber Then
+            Console.WriteLine("First Number is bigger.")
+        ElseIf firstNumber < secondNumber Then
+            Console.WriteLine("First Number is smaller.")
+        ElseIf firstNumber <= secondNumber Then
+            Console.WriteLine("First number is smaller or equal to.")
+        ElseIf firstNumber >= secondNumber Then
+            Console.WriteLine("First number is bigger or equal to.")
+        ElseIf firstNumber <> secondNumber Then
+            Console.WriteLine("The numbers are not equal.")
+        Else
+            Console.WriteLine("False")
+        End If
+        Console.ReadLine()
+
+    End Sub
+
+End Module
