72cc2cb4c30e640991407202ed34121a064970fb
Nathan C <nathan.r.comstock@gmail.com>
2020-01-28 08:39:05 -0700

    Created Loop

--- LoopExample/LoopExample.vb
+++ LoopExample/LoopExample.vb
@@ -1,6 +1,28 @@
-﻿Module LoopExample
+﻿'Nathan Comstock
+'RCET0265
+'Spring 2020
+'InsertAssignmentName
+'https://github.com/comsnath/NC-VS-S20
+
+Option Explicit On
+Option Strict On
+Module LoopExample
 
     Sub Main()
+        Dim numOne As Integer
+        Dim numTwo As Integer
+        Dim result As Integer
+        numOne = 5
+        numTwo = 7
+
+        result = numOne + numTwo
+
+        For i = 0 To 1000
+            result = result + 3
+        Next
+
+        Console.WriteLine(result)
+        Console.ReadLine()
 
     End Sub
 
