1c0b6cff7da6c5520765825464af202c98c54cc3
Nathan C <nathan.r.comstock@gmail.com>
2020-01-29 08:59:35 -0700

    Added Exception Catch throughout

--- SimpleCalculatorv2/SimpleCalculatorv2.vb
+++ SimpleCalculatorv2/SimpleCalculatorv2.vb
@@ -13,25 +13,75 @@ Module SimpleCalculatorv2
         Dim numOne As Integer
         Dim numTwo As Integer
         Dim selectAction As String
+        Dim quitChoice As Boolean
+        Dim promptUser As Boolean
 
-        'Beginning Data
-        Console.WriteLine("Simple Calculator V2. Please enter your first number.")
-        numOne = CInt(Console.ReadLine())
-        Console.WriteLine("Please enter your second number.")
-        numTwo = CInt(Console.ReadLine())
-        Console.WriteLine("Would you like to Multiply or Add?")
-        Console.WriteLine("Type + for addition.")
-        Console.WriteLine("Type * for multiplication.")
-        selectAction = Console.ReadLine()
-        'Action Selection
-        If selectAction = "+" Then
-            Console.WriteLine(numOne & " + " & numTwo & " = " & numOne + numTwo)
-        ElseIf selectAction = "*" Then
-            Console.WriteLine(numOne & " * " & numTwo & " = " & numOne * numTwo)
-        Else
-            Console.WriteLine("Error. Please try again.")
-        End If
-        Console.ReadLine()
+        promptUser = True
+        quitChoice = True
+
+        Do While quitChoice = True
+
+            'Beginning Data
+            Console.WriteLine("Simple Calculator V2. Please enter your first number.")
+            Do While promptUser = True
+                Try
+                    numOne = CInt(Console.ReadLine())
+                    promptUser = False
+                Catch ex As Exception
+                    Console.WriteLine("Error. Please try again.")
+                    promptUser = True
+                End Try
+            Loop
+
+            promptUser = True
+
+            Console.WriteLine("Please enter your second number.")
+            Do While promptUser = True
+                Try
+                    numTwo = CInt(Console.ReadLine())
+                    promptUser = False
+                Catch ex As Exception
+                    Console.WriteLine("Error. Please try again.")
+                    promptUser = True
+                End Try
+            Loop
+
+            promptUser = True
+
+            'Action Selection
+            Do While promptUser = True
+                Console.WriteLine("Would you like to Multiply or Add?")
+                Console.WriteLine("Type + for addition.")
+                Console.WriteLine("Type * for multiplication.")
+                selectAction = Console.ReadLine()
+                If selectAction = "+" Then
+                    'Try
+                    Console.WriteLine(numOne & " + " & numTwo & " = " & numOne + numTwo)
+                    'Catch ex As Exception
+                    '    Console.WriteLine("Error. Please try again.")
+                    'End Try
+                    promptUser = False
+                ElseIf selectAction = "*" Then
+                    'Try
+                    Console.WriteLine(numOne & " * " & numTwo & " = " & numOne * numTwo)
+                    'Catch ex As Exception
+                    '    Console.WriteLine("Error. Please try again.")
+                    'End Try
+                    promptUser = False
+                Else
+                    Console.WriteLine("Error. Please try again.")
+                    promptUser = True
+                End If
+            Loop
+            Console.WriteLine("Press any key to try again. Press Q to quit.")
+                If Console.ReadLine() = "Q" Then
+                    quitChoice = False
+                Else
+                    quitChoice = True
+                End If
+                Console.Clear()
+            Loop
+
     End Sub
 
 End Module
