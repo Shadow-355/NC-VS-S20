2a4dcd1e9a25c72a46632216cb3ca4c700dcd2f9
Nathan C <nathan.r.comstock@gmail.com>
2020-01-30 08:58:47 -0700

    Create simple sub example

---
+++ SubFunctionExample/SubFunction.vb
@@ -0,0 +1,38 @@
+﻿'Nathan Comstock
+'RCET0265
+'Spring 2020
+'SubFunctionExample
+'https://github.com/comsnath/NC-VS-S20
+
+Option Strict On
+Option Explicit On
+
+Module SubFunction
+
+    Sub Main()
+        Dim myNumber As Integer
+        myNumber = 7
+        Console.WriteLine("In Main")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+        Sub2(myNumber)
+    End Sub
+
+    Sub Sub2(myNumber As Integer)
+        'Dim myNumber As Integer
+        'myNumber = 7
+        Console.WriteLine("In Sub2")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+    End Sub
+
+    Function Function1() As Integer
+        Dim number As Integer
+        number = 7
+        Console.WriteLine("In Function1")
+        Console.WriteLine(number)
+        Return 0
+
+    End Function
+
+End Module
