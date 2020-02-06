f1fc53fbec96cedd5e07433e22a798685da9cdfc
Nathan C <nathan.r.comstock@gmail.com>
2020-02-06 08:55:08 -0700

    Time Table Example

--- Format Example/FormatExample.vb
+++ Format Example/FormatExample.vb
@@ -7,15 +7,41 @@
 Module FormatExample
 
     Sub Main()
-        Dim valueInt As Integer
-        valueInt = 10
-        valueInt.ToString("C")
+        'Dim valueInt As Decimal
+        'valueInt = CDec(25.5)
+        'valueInt.ToString("C")
 
-        Console.WriteLine(valueInt)
-        Console.WriteLine(valueInt.ToString("C"))
+        'Console.WriteLine(valueInt)
+        'Console.WriteLine(valueInt.ToString("C"))
+        'Console.ReadLine()
+
+        'TooManySpaces()
+        TimeTable()
+
+    End Sub
+
+    Sub TooManySpaces()
+        Dim message As String
+        message = "    Hello     "
+
+        Console.WriteLine(Len(message))
+        message = Trim(message)
+        Console.WriteLine(Len(message))
         Console.ReadLine()
+    End Sub
 
+    Sub TimeTable()
+        Dim result As Integer
 
+        For i = 1 To 10
+
+            For j = 1 To 10
+                result = i * j
+                Console.Write(result.ToString.PadLeft(5))
+            Next
+            Console.WriteLine()
+        Next
+        Console.ReadLine()
     End Sub
 
 End Module
