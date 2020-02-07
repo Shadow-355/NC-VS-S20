b03dfb15a1ed0fd9455f343a8b5eff0ea2cab037
Nathan C <nathan.r.comstock@gmail.com>
2020-02-07 08:08:10 -0700

    Better Calculator Start

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -7,10 +7,77 @@
 Module BetterCalculator
 
     Sub Main()
-        Dim numberOne As Integer
-        Dim numberTwo As Integer
+        'Variables
+        Dim numOne As Integer
+        Dim numTwo As Integer
+        Dim selectAction As String
+        Dim quitChoice As Boolean
+        Dim promptUser As Boolean
 
-        Console.WriteLine()
+        promptUser = True
+        quitChoice = True
+
+        Do While quitChoice = True
+
+            'Beginning Data
+            Console.WriteLine("Please enter two numbers. Enter ""Q"" at any time to quit.")
+            Try
+                    numOne = CInt(Console.ReadLine())
+                    promptUser = False
+                Catch ex As Exception
+                    Console.WriteLine("Error. Please try again.")
+                    promptUser = True
+                End Try
+            Loop
+
+            promptUser = True
+
+            Console.WriteLine("Please enter your second number.")
+            Do While promptUser = True
+                Try
+                    numTwo = CInt(Console.ReadLine())
+                    promptUser = False
+                Catch ex As Exception
+                    Console.WriteLine("Error. Please try again.")
+                    promptUser = True
+                End Try
+            Loop
+
+            promptUser = True
+
+            'Action Selection
+            Do While promptUser = True
+                Console.WriteLine("Would you like to Multiply or Add?")
+                Console.WriteLine("Type + for addition.")
+                Console.WriteLine("Type * for multiplication.")
+                selectAction = Console.ReadLine()
+                If selectAction = "+" Then
+                    'Try
+                    Console.WriteLine(numOne & " + " & numTwo & " = " & numOne + numTwo)
+                    'Catch ex As Exception
+                    '    Console.WriteLine("Error. Please try again.")
+                    'End Try
+                    promptUser = False
+                ElseIf selectAction = "*" Then
+                    'Try
+                    Console.WriteLine(numOne & " * " & numTwo & " = " & numOne * numTwo)
+                    'Catch ex As Exception
+                    '    Console.WriteLine("Error. Please try again.")
+                    'End Try
+                    promptUser = False
+                Else
+                    Console.WriteLine("Error. Please try again.")
+                    promptUser = True
+                End If
+            Loop
+            Console.WriteLine("Press any key to try again. Press Q to quit.")
+            If Console.ReadLine() = "Q" Then
+                quitChoice = False
+            Else
+                quitChoice = True
+            End If
+        Console.Clear()
+
 
 
 
