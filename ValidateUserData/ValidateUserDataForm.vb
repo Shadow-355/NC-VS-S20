d76f85783b3280c4746faed278d01d67f3f7881e
Nathan C <nathan.r.comstock@gmail.com>
2020-02-18 08:30:09 -0700

    Variable Scope Creation

--- ValidateUserData/ValidateUserDataForm.vb
+++ ValidateUserData/ValidateUserDataForm.vb
@@ -2,8 +2,18 @@
     Private Sub UpdateButton_Click(sender As Object, e As EventArgs) Handles UpdateButton.Click
 
         If TextBox1.Text = "" Then
-            MsgBox("Please Enter Text")
+            MsgBox("Please Enter Text" & vbNewLine)
             TextBox1.Select()
+        End If
+
+        If TextBox2.Text = "" Then
+            MsgBox("Please Enter Text" & vbNewLine)
+            TextBox2.Select()
+        End If
+
+        If TextBox3.Text = "" Then
+            MsgBox("Please Enter Text" & vbNewLine)
+            TextBox3.Select()
         End If
 
     End Sub
---
+++ VariableScopeLifetime/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+    Dim aName As String
+    'aName = "Chucks"
+
+
+    Sub Main()
+        Dim aNumber As Integer
+        aNumber = 5
+        'Console.WriteLine(aNumber)
+        Console.WriteLine(aName)
+        aName = "Chucky"
+
+        MySub()
+        'Console.WriteLine(aNumber)
+        Console.WriteLine(aName)
+        Console.ReadLine()
+    End Sub
+
+    Sub MySub()
+        Dim aNumber As Integer
+        aNumber = 5
+
+        aName = "Billy"
+    End Sub
+
+End Module
