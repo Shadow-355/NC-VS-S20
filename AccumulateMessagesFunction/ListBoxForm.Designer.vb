23e40b2b84a5d09dd9b28c16f0d9e303ac83a198
Nathan C <nathan.r.comstock@gmail.com>
2020-02-24 08:20:24 -0700

    GUI Created

---
+++ AccumulateMessagesFunction/ListBoxForm.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ListBoxForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OutputListBox = New System.Windows.Forms.ListBox()
+        Me.AddButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'OutputListBox
+        '
+        Me.OutputListBox.FormattingEnabled = True
+        Me.OutputListBox.Location = New System.Drawing.Point(335, 12)
+        Me.OutputListBox.Name = "OutputListBox"
+        Me.OutputListBox.Size = New System.Drawing.Size(453, 329)
+        Me.OutputListBox.TabIndex = 0
+        '
+        'AddButton
+        '
+        Me.AddButton.Location = New System.Drawing.Point(335, 348)
+        Me.AddButton.Name = "AddButton"
+        Me.AddButton.Size = New System.Drawing.Size(147, 90)
+        Me.AddButton.TabIndex = 1
+        Me.AddButton.Text = "&Add Item"
+        Me.AddButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(488, 348)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(147, 90)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "&Clear All"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ExitButton.Location = New System.Drawing.Point(641, 348)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(147, 90)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "&Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ListBoxForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ExitButton
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.AddButton)
+        Me.Controls.Add(Me.OutputListBox)
+        Me.Name = "ListBoxForm"
+        Me.Text = "List Box Form"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents OutputListBox As ListBox
+    Friend WithEvents AddButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
