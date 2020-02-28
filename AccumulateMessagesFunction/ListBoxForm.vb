38fd6b955f0f98642a7e3a3bc98a74341465fe46
Nathan C <nathan.r.comstock@gmail.com>
2020-02-28 08:38:11 -0700

    Created Accumulate Message Function

--- AccumulateMessagesFunction/ListBoxForm.vb
+++ AccumulateMessagesFunction/ListBoxForm.vb
@@ -1,11 +1,20 @@
 ﻿Public Class ListBoxForm
 
     Private Sub ListBoxForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'Preloaded Data
+        FirstNameTextBox.Text = "Joe"
+        LastNameTextBox.Text = "Smith"
+
+
+
 
     End Sub
 
     Private Sub AddButton_Click(sender As Object, e As EventArgs) Handles AddButton.Click
         OutputListBox.Items.Add(FirstNameTextBox.Text & LastNameTextBox.Text)
+
+
+
     End Sub
 
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
@@ -15,4 +24,21 @@
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
         OutputListBox.Items.Clear()
     End Sub
+
+    Private Sub OutputListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles OutputListBox.SelectedIndexChanged
+        Dim indexNum As Integer
+
+        Me.Text = OutputListBox.SelectedIndex.ToString
+        indexNum = InStr(OutputListBox.SelectedItem.ToString, " ")
+
+        FirstNameTextBox.Text = Trim(Strings.Left(OutputListBox.SelectedItem.ToString, indexNum))
+        LastNameTextBox.Text = Trim(Strings.Mid(OutputListBox.SelectedItem.ToString, indexNum))
+    End Sub
+    Function UserMessages(addMessage As Boolean, message As String, clearMessage As Boolean) As String
+
+        'your code here
+
+        Return formattedMessages
+
+    End Function
 End Class
---
+++ AccumulateMessagesFunctionRetry/Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim messageOne As String
+        Dim messageTwo As String
+        Dim messageThree As String
+
+
+
+        Console.WriteLine("Enter your first string:")
+        messageOne = Console.ReadLine()
+        Console.WriteLine("Enter your second string:")
+        messageTwo = Console.ReadLine()
+        Console.WriteLine("Enter your third string:")
+        messageThree = Console.ReadLine()
+        Console.WriteLine("Processing...")
+
+        UserMessages(True, messageOne, False)
+        UserMessages(True, messageTwo, False)
+        UserMessages(True, messageThree, False)
+
+        Console.ReadLine()
+    End Sub
+    Function UserMessages(addMessage As Boolean, message As String, clearMessage As Boolean) As String
+        Static formattedMessages As String
+
+        formattedMessages &= message & vbNewLine
+
+
+        Console.WriteLine(message)
+        Return formattedMessages
+
+    End Function
+End Module
