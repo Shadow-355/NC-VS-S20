0c03146e9d958dd06dfd072eb5756daa40b55d49
Nathan C <nathan.r.comstock@gmail.com>
2020-03-03 08:51:24 -0700

    Created Example Array

---
+++ ArrayExample/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim array(3) As Integer
+
+        array(1) = 5
+
+        array = {1, 2, 3, 4}
+
+        array(0) = 5
+        ReDim Preserve array(15)
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
