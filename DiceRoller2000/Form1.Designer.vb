3bec199e8274cbd9545009d9f44f92286b232941
Nathan C <nathan.r.comstock@gmail.com>
2020-03-11 08:22:07 -0600

    Created DiceRoller2000, built form, and added basic code.

---
+++ DiceRoller2000/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OutputListBox = New System.Windows.Forms.ListBox()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'OutputListBox
+        '
+        Me.OutputListBox.FormattingEnabled = True
+        Me.OutputListBox.Location = New System.Drawing.Point(13, 13)
+        Me.OutputListBox.Name = "OutputListBox"
+        Me.OutputListBox.Size = New System.Drawing.Size(417, 186)
+        Me.OutputListBox.TabIndex = 0
+        '
+        'RollButton
+        '
+        Me.RollButton.Location = New System.Drawing.Point(13, 205)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(417, 23)
+        Me.RollButton.TabIndex = 1
+        Me.RollButton.Text = "Roll!"
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(445, 242)
+        Me.Controls.Add(Me.RollButton)
+        Me.Controls.Add(Me.OutputListBox)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents OutputListBox As ListBox
+    Friend WithEvents RollButton As Button
+End Class
---
+++ DiceRoller2000/Form1.vb
@@ -0,0 +1,79 @@
+﻿'Nathan Comstock
+'RCET0265
+'Spring 2020
+'DiceRoller2000
+'https://github.com/comsnath/NC-VS-S20
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+    End Sub
+
+
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+        'Create variables
+        Dim firstDice As Single
+        Dim secondDice As Single
+        Dim sumDice As Single
+        Dim diceArray(12) As Integer
+
+        'Set Array to 0
+        diceArray(2) = 0
+        diceArray(3) = 0
+        diceArray(4) = 0
+        diceArray(5) = 0
+        diceArray(6) = 0
+        diceArray(7) = 0
+        diceArray(8) = 0
+        diceArray(9) = 0
+        diceArray(10) = 0
+        diceArray(11) = 0
+        diceArray(12) = 0
+
+        'Start Roller
+        For roll = 1 To 1000
+            'Randomize
+            Randomize()
+            firstDice = Int((6 * Rnd()) + 1)
+            Randomize()
+            secondDice = Int((6 * Rnd()) + 1)
+            sumDice = firstDice + secondDice
+
+            'Check Number
+            If sumDice = 2 Then
+                diceArray(2) += 1
+            ElseIf sumDice = 3 Then
+                diceArray(3) += 1
+            ElseIf sumDice = 4 Then
+                diceArray(4) += 1
+            ElseIf sumDice = 5 Then
+                diceArray(5) += 1
+            ElseIf sumDice = 6 Then
+                diceArray(6) += 1
+            ElseIf sumDice = 7 Then
+                diceArray(7) += 1
+            ElseIf sumDice = 8 Then
+                diceArray(8) += 1
+            ElseIf sumDice = 9 Then
+                diceArray(9) += 1
+            ElseIf sumDice = 10 Then
+                diceArray(10) += 1
+            ElseIf sumDice = 12 Then
+                diceArray(11) += 1
+            ElseIf sumDice = 11 Then
+                diceArray(12) += 1
+            End If
+        Next
+
+        'Write out Array
+
+        OutputListBox.Text = "2,  3,  4,  5,   6,  7,   8,   9,  10,  11,  12" & vbNewLine
+        OutputListBox.Text &= diceArray(2) & ", " & diceArray(3) & ", " & diceArray(4) & ", " & diceArray(5) _
+                         & ", " & diceArray(6) & ", " & diceArray(7) & ", " & diceArray(8) & ", " & diceArray(9) _
+                         & ", " & diceArray(10) & ", " & diceArray(11) & ", " & diceArray(12)
+
+    End Sub
+
+
+End Class
