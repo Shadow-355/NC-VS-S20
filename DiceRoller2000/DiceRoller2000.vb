e9663e1a0fb353b9242a18e581588db1ea9f2588
Nathan C <nathan.r.comstock@gmail.com>
2020-03-12 09:11:48 -0600

    Condensed and fixed code

--- DiceRoller2000/DiceRoller2000.vb
+++ DiceRoller2000/DiceRoller2000.vb
@@ -18,19 +18,6 @@ Public Class DiceRollerForm
         Dim sumDice As Single
         Dim diceArray(12) As Integer
 
-        'Set Array to 0
-        diceArray(2) = 0
-        diceArray(3) = 0
-        diceArray(4) = 0
-        diceArray(5) = 0
-        diceArray(6) = 0
-        diceArray(7) = 0
-        diceArray(8) = 0
-        diceArray(9) = 0
-        diceArray(10) = 0
-        diceArray(11) = 0
-        diceArray(12) = 0
-
         'Start Roller
         For roll = 1 To 1000
             'Randomize
@@ -40,55 +27,23 @@ Public Class DiceRollerForm
             secondDice = Int((6 * Rnd()) + 1)
             sumDice = firstDice + secondDice
 
-            'Check Number
-            If sumDice = 2 Then
-                diceArray(2) += 1
-            ElseIf sumDice = 3 Then
-                diceArray(3) += 1
-            ElseIf sumDice = 4 Then
-                diceArray(4) += 1
-            ElseIf sumDice = 5 Then
-                diceArray(5) += 1
-            ElseIf sumDice = 6 Then
-                diceArray(6) += 1
-            ElseIf sumDice = 7 Then
-                diceArray(7) += 1
-            ElseIf sumDice = 8 Then
-                diceArray(8) += 1
-            ElseIf sumDice = 9 Then
-                diceArray(9) += 1
-            ElseIf sumDice = 10 Then
-                diceArray(10) += 1
-            ElseIf sumDice = 12 Then
-                diceArray(11) += 1
-            ElseIf sumDice = 11 Then
-                diceArray(12) += 1
-            End If
+            'Check Number and Insert Into Array
+            Select Case sumDice
+                Case sumDice
+                    diceArray(sumDice) += 1
+            End Select
+
         Next
+
+
+
 
         'Write out Array
-
-        OutputListBox.Text = "2,  3,  4,  5,   6,  7,   8,   9,  10,  11,  12" & vbNewLine
-        OutputListBox.Text &= diceArray(2) & ", " & diceArray(3) & ", " & diceArray(4) & ", " & diceArray(5) _
+        OutputListBox.Items.Add("2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12")
+        OutputListBox.Items.Add(diceArray(2) & ", " & diceArray(3) & ", " & diceArray(4) & ", " & diceArray(5) _
                          & ", " & diceArray(6) & ", " & diceArray(7) & ", " & diceArray(8) & ", " & diceArray(9) _
-                         & ", " & diceArray(10) & ", " & diceArray(11) & ", " & diceArray(12)
-
-
-
-        Console.WriteLine(Space(19) & Roll)
-        For i = LBound(temp) To UBound(temp)
-            Console.Write(CStr(temp(i)).PadLeft(4) & "|")
-        Next
-        Console.WriteLine(StrDup(55, "-"))
-
-
-
-
-
-
-
-
-
+                         & ", " & diceArray(10) & ", " & diceArray(11) & ", " & diceArray(12))
+        OutputListBox.Items.Add(StrDup(55, "-"))
     End Sub
 
 
